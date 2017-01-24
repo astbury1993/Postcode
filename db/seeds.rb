3aee62010bb878061cd7b2b490542335fdbe9829
astbury1993 <curtis.astbury@outlook.com>
2017-01-24 17:50:04 +0000

    Preparing an initial data import
    I am preparing a data import so committing now before performing the
    necessary actions in case I need to rollback. In here I have created a
    TestScript ruby file in a new folder called jobs. I will execute this
    at some point in console. I have also added a unit_test csv which
    contains some working and some failing postcodes. I have also prepared
    my seeds.rb file which is used for importing an initial set of data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib, 'seeds', 'unit_test.csv'))
+puts csv_text
