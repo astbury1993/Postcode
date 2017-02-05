d4260778981eaaf53c92b3e4326cf58eb667c408
astbury1993 <curtis.astbury@outlook.com>
2017-02-05 19:14:21 +0000

    Attempt to use PumaWorkerKiller in Heroku
    Heroku is still failing often so I am attempting to use PumaWorkerKiller

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,18 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
+
+
+before_fork do
+    PumaWorkerKiller.config do |config|
+        config.ram           = 1024 # mb
+        config.frequency     = 5    # seconds
+        config.percent_usage = 0.98
+        config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
+    end
+    PumaWorkerKiller.start
+end
+
 preload_app!
 
 rackup      DefaultRackup
