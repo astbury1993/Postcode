40d559fde8ef8bb57224cb4eeb2f0184560b592d
astbury1993 <curtis.astbury@outlook.com>
2017-02-05 12:47:19 +0000

    Remove 'Put' line
    This should improve the performance on Heroku as the console won’t have
    to output this line for every record.

--- app/models/import.rb
+++ app/models/import.rb
@@ -19,7 +19,7 @@ def self.import(file)
     value = row[1]
     
     #Output to console - remove in production
-    puts value
+    # puts value
     
     #Add to the pass or fail array
     if value =~ /(GIR\s0AA)|((([A-PR-UWYZ][0-9][0-9]?)|(([A-PR-UWYZ][A-HK-Y][0-9](?!(BR|FY|HA|HD|HG|HR|HS|HX|JE|LD|SM|SR|WC|WN|ZE)[0-9])[0-9])|([A-PR-UWYZ][A-HK-Y](?!AB|LL|SO)[0-9]) |(WC[0-9][A-Z]) |(([A-PR-UWYZ][0-9][A-HJKPSTUW]) |([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))))\s[0-9][ABD-HJLNP-UW-Z]{2})/
